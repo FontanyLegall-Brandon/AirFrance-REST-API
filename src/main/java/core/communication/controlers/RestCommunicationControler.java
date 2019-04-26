d41d09f717a577c7c2778cc9fa728e47546e0fd8
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2019-04-26 21:53:17 +0200

    get user

--- src/main/java/core/communication/controlers/RestCommunicationControler.java
+++ src/main/java/core/communication/controlers/RestCommunicationControler.java
@@ -4,11 +4,16 @@ import core.AppEngine;
 import core.communication.container.UserCountainer;
 import core.data.User;
 import core.data.exceptions.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 
 @RestController
@@ -18,28 +23,39 @@ public class RestCommunicationControler{
     @Autowired
     AppEngine appEngine;
 
-    @GetMapping("/test/")
-    public String getPlateau(){
-        return new String("test");
-    }
-
     @PostMapping(value = "/register")
-    @ExceptionHandler({ InvalidEmailException.class,EmptyFieldException.class,NonUniqueEmailException.class,BadGeographicLocationException.class,InvalidEmailException.class,MinimumAgeRequiredException.class })
     public User resgister(@RequestBody User user) throws InvalidEmailException, EmptyFieldException, NonUniqueEmailException, BadGeographicLocationException, InvalidPhoneNumberException, MinimumAgeRequiredException {
 
-        long start = System.currentTimeMillis();
+        long start = System.currentTimeMillis(); //process time log
         User createdUser = new User(user);
 
         //If User created
         appEngine.addUser(createdUser);
         log.info("New User created "+createdUser.toString());
-        long finish = System.currentTimeMillis();
-        long timeElapsed = finish - start;
-        log.info("Processing time : "+timeElapsed+"ms");
+        long finish = System.currentTimeMillis(); //process time log
+        long timeElapsed = finish - start; //process time log
+        log.info("User creation processing time : "+timeElapsed+"ms"); //process time log
         return createdUser;
 
     }
 
+    @GetMapping(value = "/registered")
+    public UserCountainer registered(){
+        return new UserCountainer(appEngine.getUserList());
+    }
+
+    @GetMapping(value = "/registered/{id}")
+    public UserCountainer registeredId(@PathVariable int id) throws UserNotFoundException {
+        long start = System.currentTimeMillis(); //process time log
+        if(appEngine.getUserList().size() <= id){
+            throw new UserNotFoundException();
+        }
+        User user = appEngine.getUserList().get(id);
+        long finish = System.currentTimeMillis(); //process time log
+        long timeElapsed = finish - start; //process time log
+        log.info("User getter processing time : "+timeElapsed+"ms"); //process time log
+        return new UserCountainer(user);
+    }
 
 
 }
